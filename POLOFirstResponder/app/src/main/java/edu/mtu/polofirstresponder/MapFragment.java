432e99a5a350660c1684bbe3c4e732764b4af61f
Jordan Kieltyka <jmkielty@mtu.edu>
2019-02-23 22:06:25 -0500

    Update MapFragment.java

--- POLOFirstResponder/app/src/main/java/edu/mtu/polofirstresponder/MapFragment.java
+++ POLOFirstResponder/app/src/main/java/edu/mtu/polofirstresponder/MapFragment.java
@@ -1,8 +1,11 @@
 package edu.mtu.polofirstresponder;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,6 +57,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         MapsInitializer.initialize(getContext());
 
         mGoogleMap = googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
     }
 }
