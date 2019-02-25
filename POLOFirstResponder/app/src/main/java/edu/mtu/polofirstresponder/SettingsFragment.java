30471e3f2d8c294a8ccacebddcb7c4567570f1c9
Jordan Kieltyka <jmkielty@mtu.edu>
2019-02-24 19:34:02 -0500

    updated app icon

--- POLOFirstResponder/app/src/main/java/edu/mtu/polofirstresponder/SettingsFragment.java
+++ POLOFirstResponder/app/src/main/java/edu/mtu/polofirstresponder/SettingsFragment.java
@@ -1,19 +1,22 @@
 package edu.mtu.polofirstresponder;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
-
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -23,8 +26,8 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
-
 }
