349cd532d65c309635206b0a6db065e643992de6
kredlebe <48255473+kredlebe@users.noreply.github.com>
2019-03-27 16:37:16 -0400

    Trying to fix crashing on phone

--- GunshotProcessor/app/src/main/java/edu/mtu/polocivilian/AudioProcessor.java
+++ GunshotProcessor/app/src/main/java/edu/mtu/polocivilian/AudioProcessor.java
@@ -49,7 +49,7 @@ public class AudioProcessor implements Runnable {
 
 //below is the new stuff we added. we can delete the gunshot document after we get everything presented.
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("Civilians").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).update("Position",(new LatLng(location.getLatitude(),location.getLongitude())));
+        db.collection("Civilians").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(new LatLng(location.getLatitude(),location.getLongitude()));
 
         if (is_gunshot) {
 
