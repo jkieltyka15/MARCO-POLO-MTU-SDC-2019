bb1fa75932229b60e5a7afc32500b1b3d88b7f41
kredlebe <48255473+kredlebe@users.noreply.github.com>
2019-04-04 13:30:38 -0400

    Fixed Toobal
    Changed color of tool bar

--- AFRLCompetitionButton/app/src/main/java/edu/mtu/afrlcompetitionbutton/MainActivity.java
+++ AFRLCompetitionButton/app/src/main/java/edu/mtu/afrlcompetitionbutton/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         INSTANCE = this;
 
         permissionManager = new PermissionManager() {
